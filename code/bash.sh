7beb8d31b9593ab7df443302bcd075345197f196
saurabhkumar04 <saurabhkumar.1004@gmail.com>
2017-12-05 00:31:41 -0500

    Add files via upload

---
+++ code/bash.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#########################################
+# Installing necessary packages
+# locally. This script will install packages
+# into the ~/local/bin directory and install
+# numpy + scipy
+#########################################
+
+pip install numpy  # install numpy
+pip install scipy
+
+pip install matplotlib  # install numpy
+pip install hashlib
+
+python blockchain.py
